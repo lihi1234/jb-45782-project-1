e44ad0e2455f4e867a6d741906789b0a814ea3cc
ליהי ברלב <lihibl5@gmail.com>
2025-07-15 20:52:33 +0300

    add delete button

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 //localStorage.removeItem("products");
 let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
-let index=0;
+let index = 0;
 
 window.onload = function () {
 
     renderList();
-    
+
 };
 
 
-function renderList(){
+function renderList() {
     //let products = JSON.parse(localStorage.getItem("products")) || [];
-    document.getElementById("tasks-list").innerHTML= "";
+    document.getElementById("tasks-list").innerHTML = "";
     tasks.forEach((item, index) => {
         const newTask = CreateNewTask(item, index);
         document.getElementById("tasks-list").innerHTML += newTask;
@@ -29,18 +29,18 @@ function addTask(event) {
     const newTask = CreateNewTask(myData, index);
     //fadeIn(newTask);
     addToList(newTask);
-   
-   index++;
+
+    index++;
     console.log(tasks);
     // form.reset();
-    saveToLocalStorage(myData);  
+    saveToLocalStorage(myData);
     clearForm();
 
 
 
 }
 
-function saveToLocalStorage(myData){
+function saveToLocalStorage(myData) {
     tasks.push(myData);
     localStorage.setItem("tasks", JSON.stringify(tasks));
 }
@@ -58,16 +58,20 @@ function getData() {
 }
 
 function CreateNewTask(myData, index) {
-      const { describe, date, time } = myData;
-    const newTask= `<div id="${index}" class="task ">
+    const { describe, date, time } = myData;
+    const newTask = `<div id="${index}" class="task ">
     <br><br>
+    <div class="delete-btn-div">
+  <button class="delete-btn" onclick="deleteTask(${index})">     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle-fill" viewBox="0 0 16 16">
+  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293z"/>
+</svg></button></div>
                     ${describe}
                     <br>
                     ${date}
                     <br>
                     ${time}
             </div>`
-            return newTask;
+    return newTask;
 }
 
 function addToList(newTask) {
@@ -80,7 +84,7 @@ function clearForm() {
     document.getElementById("task-form").reset();
 }
 
-function deleteTask(index){
+function deleteTask(index) {
 
     // let animals = JSON.parse(localStorage.getItem("animals") || "[]");
     tasks.splice(index, 1); // Remove item at index
@@ -88,10 +92,10 @@ function deleteTask(index){
     renderList(); // Refresh table
 }
 
- function fadeIn(index) {
+function fadeIn(index) {
     //tasks[index].classList.add("fade-in");
     index.classList.add("fade-in");
-  }
+}
 
 
 
