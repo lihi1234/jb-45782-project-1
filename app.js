d8a7d4dc584ac0feaae116dafe0750a82f2e3c25
ליהי ברלב <lihibl5@gmail.com>
2025-07-22 11:29:44 +0300

    fade in fixed

--- app.js
+++ app.js
@@ -27,8 +27,9 @@ function addTask(event) {
     event.preventDefault();
     const myData = getData();
     const newTask = CreateNewTask(myData, index);
-    //fadeIn(newTask);
-    addToList(newTask);
+    // fadeIn(newTask);
+
+    addToList(newTask, true);
 
     index++;
     console.log(tasks);
@@ -59,10 +60,12 @@ function getData() {
 
 function CreateNewTask(myData, index) {
     const { describe, date, time } = myData;
+
     const newTask = `<div id="${index}" class="task ">
-    <br><br>
+    
     <div class="delete-btn-div">
-  <button class="delete-btn" onclick="deleteTask(${index})">     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle-fill" viewBox="0 0 16 16">
+  <button class="delete-btn" onclick="deleteTask(${index})">  
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle-fill" viewBox="0 0 16 16">
   <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293z"/>
 </svg></button></div>
                     ${describe}
@@ -71,12 +74,20 @@ function CreateNewTask(myData, index) {
                     <br>
                     ${time}
             </div>`
+
     return newTask;
 }
 
-function addToList(newTask) {
+function addToList(newTask, isAnimation) {
+         const taskContainer = document.createElement("task-container");
 
-    document.getElementById("tasks-list").innerHTML += newTask;
+    if (isAnimation) {
+   
+        taskContainer.classList.add("fade-in");
+    }
+    taskContainer.innerHTML+=newTask;
+    document.getElementById("tasks-list").appendChild(taskContainer);
+
 
 }
 
@@ -92,10 +103,6 @@ function deleteTask(index) {
     renderList(); // Refresh table
 }
 
-function fadeIn(index) {
-    //tasks[index].classList.add("fade-in");
-    index.classList.add("fade-in");
-}
 
 
 
